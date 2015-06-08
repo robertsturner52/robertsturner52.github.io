fe3cb5f5e76828bd1c49eebd8de21521f86f1297
robertsturner52 <robertsturner52@gmail.com>
2015-06-08 16:25:50 -0700

    info

---
+++ script.js
@@ -0,0 +1,18 @@
+var myLinks = [
+	"https://www.facebook.com/robertstuart.turner/",
+	"https://www.facebook.com/joshuawoodward",
+	"https://www.facebook.com/jemmy.bluestein",
+	"https://www.facebook.com/Jenlo1408",
+	"https://www.facebook.com/adrianboisclair"
+];
+/*var i = 0;
+console.log('<ul>');
+while (i < myLinks.length) {
+console.log('   <li><a href="'+myLinks[i]+'><'+i+'></li>');
+i = i+1;
+}
+console.log('</ul>');*/
+
+for (var i = 0; i < myLinks.length; i++) {
+	console.log(myLinks[i]);
+}
